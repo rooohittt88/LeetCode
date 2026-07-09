7a1c2fd470f33a415222942b4a5a1e5fe2c4a219
Rohit sharma <sharmaixbrohit@gmail.com>
2026-07-09 17:29:10 +0530

    Refactor reverse string logic and add user input
    Refactored the reverse string logic into the main method and added user input functionality.

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,29 @@
-class Solution {
-    public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length - 1;
-        while (left < right) {
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
+//brute force approach according to me
+import java.util.*;
+class Mainn {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Please enter a word: ");
+        String word=in.next();
+        char[] arr=new char[word.length()];
+        for(int i=0;i<word.length();i++){
+            arr[i]=word.charAt(i);
         }
+        int len=word.length();
+        int start=0;
+        int end=len-1;
+        // System.out.println(Arrays.toString(arr));
+
+
+        for(int i=0;i<end;i++){
+            char temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(arr));
+        
+        
     }
 }
