cb95d22fb0d7b91e55cbe6f31923b010cbd638a8
Rohit sharma <sharmaixbrohit@gmail.com>
2026-07-14 19:28:54 +0530

    Time: 0 ms (100%), Space: 48.3 MB (22.68%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public double myPow(double x, int n) {
+        // double ans=0;
+        // if (n >= 0) {
+        //     ans = Math.pow(x, n);
+        // }
+        // double anss=0;
+        // if(n<1){
+        // double sign=Math.abs(n);
+        // ans=1/(Math.pow(x,n));
+        // }
+        return Math.pow(x,n);
+    }
+}
