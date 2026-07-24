9b3887525d7f90fc9417fd09a392d8db064eea9c
Rohit sharma <sharmaixbrohit@gmail.com>
2026-07-24 18:11:55 +0530

    Time: 3 ms (54.57%), Space: 46.3 MB (63.78%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        Arrays.sort(prices);
+        int sum = prices[0] + prices[1];
+        int ans = sum - money;
+        if (sum <= money) return money-sum;
+        else return money;
+    }
+}
