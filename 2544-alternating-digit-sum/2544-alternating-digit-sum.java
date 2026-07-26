fd040776a52d7928d3bc60879f4b1a7e629a94ae
Rohit sharma <sharmaixbrohit@gmail.com>
2026-07-26 18:20:25 +0530

    Time: 0 ms (100%), Space: 42.1 MB (49.27%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+        int sum = 0;
+        String s = Integer.toString(n);
+        for (int i = 0; i < s.length(); i++) {
+            int digit = s.charAt(i) - '0';
+            if (i % 2 == 0) {
+                sum += digit;
+            } else {
+                sum -= digit;
+            }
+        }
+        return sum;
+    }
+}
