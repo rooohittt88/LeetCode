e8c7c8dc608511d8830e5a15606eba2ed122a3ce
Rohit sharma <sharmaixbrohit@gmail.com>
2026-07-30 18:11:48 +0530

    Time: 9 ms (47.22%), Space: 42.8 MB (28.47%) - LeetHub

---
+++ 3790-smallest-all-ones-multiple/3790-smallest-all-ones-multiple.java
@@ -0,0 +1,12 @@
+class Solution { 
+    public int minAllOneMultiple(int k) { 
+        if (k % 2 == 0 || k % 5 == 0) return -1; 
+        int rem = 1; 
+        int len = 1; 
+        while (rem % k != 0) { 
+            rem = (rem * 10 + 1) % k; 
+            len++;
+        } 
+        return len; 
+    } 
+}
